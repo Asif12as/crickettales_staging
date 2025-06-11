143213926d6007986923e986e6074ba9721d38e7
Asif12as <mda957947@gmail.com>
2025-06-11 16:34:57 +0530

    Fix Vercel deployment configuration

---
+++ api/index.js
@@ -0,0 +1,12 @@
+// API index file
+// This file serves as an entry point for API routes
+
+export { default as stories } from './stories';
+export { default as storySubmit } from './story-submit';
+export { default as storyVote } from './story-vote';
+export { default as boostStatus } from './boost-status';
+export { default as createBoostSession } from './create-boost-session';
+export { default as createPrioritySession } from './create-priority-session';
+export { default as createVotePackSession } from './create-vote-pack-session';
+export { default as proMetrics } from './pro-metrics';
+export { default as proTrial } from './pro-trial';
